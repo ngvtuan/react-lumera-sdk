98544a615c5b0e4d4c877d0183a20683fdd7490d
Tuan Nguyen <titan.ngvtuan@gmail.com>
2025-12-05 12:12:56 +0700

    update plugin

--- src/hooks/useLumeraClient.ts
+++ src/hooks/useLumeraClient.ts
@@ -2,168 +2,265 @@
 import * as lumeraClient from "@lumera-protocol/sdk-js";
 import type { EncodeObject } from "@cosmjs/proto-signing";
 
-import { ILumeraClient, IDownload, IUpload } from '../types';
+import {
+  ILumeraClient,
+  IDownload,
+  IUpload,
+  ILumeraClientWithoutSigner,
+} from '../types';
 
-export const useLumeraClient = () => {
-  const createBatchedSignaturePrompter = () => {
-    return lumeraClient.createBatchedSignaturePrompter();
+const createBatchedSignaturePrompter = () => {
+  return lumeraClient.createBatchedSignaturePrompter();
+}
+
+const createDefaultTxPrompter = () => {
+  return lumeraClient.createDefaultTxPrompter();
+}
+
+const getLumeraClient = async (config: ILumeraClient) => {
+  const {
+    chainId,
+    rpcUrl,
+    lcdUrl,
+    snapiUrl,
+    signer,
+    address,
+    gasPrice = '0.025ulume',
+    timeout = 45000,
+    maxRetries = 3,
+  } = config;
+  if (!chainId) {
+    throw new Error('Chain ID is required.');
   }
-
-  const createDefaultTxPrompter = () => {
-    return lumeraClient.createDefaultTxPrompter();
+  if (!rpcUrl) {
+    throw new Error('RPC Url is required.');
   }
+  if (!lcdUrl) {
+    throw new Error('LCD Url is required.');
+  }
+  if (!snapiUrl) {
+    throw new Error('Snapi Url is required.');
+  }
+  if (!signer) {
+    throw new Error('Signer is required.');
+  }
+  if (!address) {
+    throw new Error('Address is required.');
+  }
+  const client = await lumeraClient.createLumeraClient({
+    chainId,
+    rpcUrl,
+    lcdUrl,
+    snapiUrl,
+    signer,
+    address,
+    gasPrice,
+    http: {
+      timeout,
+      maxRetries,
+    },
+  });
+  return client;
+}
 
-  const getLumeraClient = async (config: ILumeraClient) => {
-    const {
-      chainId,
-      rpcUrl,
-      lcdUrl,
-      snapiUrl,
-      signer,
-      address,
-      gasPrice = '0.025ulume',
-      timeout = 45000,
-      maxRetries = 3,
-    } = config;
-    if (!chainId || !rpcUrl || !lcdUrl || !snapiUrl || !signer) {
-      return null
+const getLumeraClientWithoutSigner = async (config: ILumeraClientWithoutSigner) => {
+  const {
+    chainId,
+    rpcUrl,
+    lcdUrl,
+    snapiUrl,
+    gasPrice = '0.025ulume',
+    timeout = 45000,
+    maxRetries = 3,
+  } = config;
+  if (!chainId) {
+    throw new Error('Chain ID is required.');
+  }
+  if (!rpcUrl) {
+    throw new Error('RPC Url is required.');
+  }
+  if (!lcdUrl) {
+    throw new Error('LCD Url is required.');
+  }
+  if (!snapiUrl) {
+    throw new Error('Snapi Url is required.');
+  }
+  const signer: any = null;
+  const client = await lumeraClient.createLumeraClient({
+    chainId,
+    rpcUrl,
+    lcdUrl,
+    snapiUrl,
+    signer,
+    address: '',
+    gasPrice,
+    http: {
+      timeout,
+      maxRetries,
+    },
+  });
+  return client;
+}
+
+const getSupernodes = async (client: lumeraClient.LumeraClient) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
     }
-    const client = await lumeraClient.createLumeraClient({
-      chainId,
-      rpcUrl,
-      lcdUrl,
-      snapiUrl,
-      signer,
-      address,
-      gasPrice,
-      http: {
-        timeout,
-        maxRetries,
-      },
+    const items = await client.Blockchain.Supernode.listSupernodes();
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const simulate = async (client: lumeraClient.LumeraClient, address: string, msgs: EncodeObject[], memo: string) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const items = await client.Blockchain.Tx.simulate(address, msgs, memo)
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const getSupernodeParams = async (client: lumeraClient.LumeraClient) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const items = await client.Blockchain.Supernode.getParams();
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const getSupernodeByAddress = async (client: lumeraClient.LumeraClient, supernodeAddress: string) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const items = await client.Blockchain.Supernode.getSupernodeByAddress(supernodeAddress);
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const getSupernode = async (client: lumeraClient.LumeraClient, validatorAddress: string) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const items = await client.Blockchain.Supernode.getSupernode(validatorAddress);
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const getKeplrSigner = async (chainId: string) => {
+  const signer: any = await lumeraClient.getKeplrSigner(chainId);
+  return signer;
+}
+
+const downloadCascade = async (item: IDownload, client: lumeraClient.LumeraClient) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const stream = await client.Cascade.downloader.download(item.lastActionId, {
+      pollInterval: item.pollInterval || 2000,
+      timeout: item.timeout || 300000,
     });
-    return client;
-  }
 
-  const getSupernodes = async (config: ILumeraClient) => {
-    try {
-      const client = await getLumeraClient(config);
-      if (!client) {
-        return null;
-      }
-      const items = await client.Blockchain.Supernode.listSupernodes();
-      return items;
-    } catch (error) {
+    return stream;
+  } catch (error) {
       throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+const uploadCascade = async (item: IUpload, client: lumeraClient.LumeraClient) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
     }
-  }
-
-  const simulate = async (config: ILumeraClient, address: string, msgs: EncodeObject[], memo: string) => {
-    try {
-      const client = await getLumeraClient(config);
-      if (!client) {
-        return null;
-      }
-      const items = await client.Blockchain.Tx.simulate(address, msgs, memo)
-      return items;
-    } catch (error) {
-      throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
-    }
-  }
-
-  const getSupernodeParams = async (config: ILumeraClient) => {
-    try {
-      const client = await getLumeraClient(config);
-      if (!client) {
-        return null;
-      }
-      const items = await client.Blockchain.Supernode.getParams();
-      return items;
-    } catch (error) {
-      throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
-    }
-  }
-
-  const getSupernodeByAddress = async (config: ILumeraClient, supernodeAddress: string) => {
-    try {
-      const client = await getLumeraClient(config);
-      if (!client) {
-        return null;
-      }
-      const items = await client.Blockchain.Supernode.getSupernodeByAddress(supernodeAddress);
-      return items;
-    } catch (error) {
-      throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
-    }
-  }
-
-  const getSupernode = async (config: ILumeraClient, validatorAddress: string) => {
-    try {
-      const client = await getLumeraClient(config);
-      if (!client) {
-        return null;
-      }
-      const items = await client.Blockchain.Supernode.getSupernode(validatorAddress);
-      return items;
-    } catch (error) {
-      throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
-    }
-  }
-
-  const getKeplrSigner = async (chainId: string) => {
-    const signer: any = await lumeraClient.getKeplrSigner(chainId);
-    return signer;
-  }
-
-  const downloadCascade = async (item: IDownload, lumeraClientConfig: ILumeraClient) => {
-    try {
-      const client = await getLumeraClient(lumeraClientConfig);
-      if (!client) {
-        return null;
-      }
-      const stream = await client.Cascade.downloader.download(item.lastActionId, {
+    const result = await client.Cascade.uploader.uploadFile(item.fileBytes, {
+      fileName: item.fileName,
+      isPublic: item.isPublic || false,
+      expirationTime: item.expirationTime || Math.floor(Date.now() / 1000 + 86400 * 1.5).toString(),
+      taskOptions: {
         pollInterval: item.pollInterval || 2000,
         timeout: item.timeout || 300000,
-      });
+      },
+      signaturePrompter: item.signaturePrompter,
+      txPrompter: item.txPrompter,
+    });
+    return result
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
 
-      return stream;
-    } catch (error) {
-        throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+const getAction = async (client: lumeraClient.LumeraClient, actionId: string) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
     }
-  }
-
-  const uploadCascade = async (item: IUpload, lumeraClientConfig: ILumeraClient) => {
-    try {
-      const client = await getLumeraClient(lumeraClientConfig);
-      if (!client) {
-        return null;
-      }
-      const result = await client.Cascade.uploader.uploadFile(item.fileBytes, {
-        fileName: item.fileName,
-        isPublic: item.isPublic || false,
-        expirationTime: item.expirationTime || Math.floor(Date.now() / 1000 + 86400 * 1.5).toString(),
-        taskOptions: {
-          pollInterval: item.pollInterval || 2000,
-          timeout: item.timeout || 300000,
-        },
-        signaturePrompter: item.signaturePrompter,
-        txPrompter: item.txPrompter,
-      });
-      return result
-    } catch (error) {
-      throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+    if (!actionId) {
+      throw new Error('actionId is required.')
     }
+    const items = await client.Blockchain.Action.getAction(actionId);
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
   }
+}
 
-  return {
-    simulate,
-    getSupernodeParams,
-    getSupernodeByAddress,
-    getSupernode,
-    createBatchedSignaturePrompter,
-    createDefaultTxPrompter,
-    getSupernodes,
-    getKeplrSigner,
-    downloadCascade,
-    uploadCascade,
+const getActionFee = async (client: lumeraClient.LumeraClient, dataSize: number) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    if (!dataSize || dataSize <= 0) {
+      throw new Error('dataSize is required.')
+    }
+    const items = await client.Blockchain.Action.getActionFee(dataSize);
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
   }
+}
+
+const getParams = async (client: lumeraClient.LumeraClient) => {
+  try {
+    if (!client) {
+      throw new Error('client is required.')
+    }
+    const items = await client.Blockchain.Action.getParams();
+    return items;
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : 'An unknown error occurred.')
+  }
+}
+
+export {
+  getAction,
+  getActionFee,
+  getParams,
+  getLumeraClient,
+  getLumeraClientWithoutSigner,
+  simulate,
+  getSupernodeParams,
+  getSupernodeByAddress,
+  getSupernode,
+  createBatchedSignaturePrompter,
+  createDefaultTxPrompter,
+  getSupernodes,
+  getKeplrSigner,
+  downloadCascade,
+  uploadCascade,
 }
--- src/types.ts
+++ src/types.ts
@@ -1,35 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SignaturePrompter, TxPrompter } from '@lumera-protocol/sdk-js';
 
 export interface ILumeraClient {
-    chainId: string;
-    rpcUrl: string;
-    lcdUrl: string;
-    snapiUrl: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    signer: any;
-    address: string;
-    gasPrice: string;
-    timeout?: number;
-    maxRetries?: number;
+  chainId: string;
+  rpcUrl: string;
+  lcdUrl: string;
+  snapiUrl: string;
+  signer: any;
+  address: string;
+  gasPrice: string;
+  timeout?: number;
+  maxRetries?: number;
+}
+
+export interface ILumeraClientWithoutSigner {
+  chainId: string;
+  rpcUrl: string;
+  lcdUrl: string;
+  snapiUrl: string;
+  gasPrice: string;
+  timeout?: number;
+  maxRetries?: number;
 }
 
 export interface IDownload {
-    lastActionId: string;
-    pollInterval?: number;
-    timeout?: number;
+  lastActionId: string;
+  pollInterval?: number;
+  timeout?: number;
 }
 
 type TSignaturePrompter = SignaturePrompter & {
-    reset: () => void;
+  reset: () => void;
 }
 
 export interface IUpload {
-    fileBytes: Uint8Array<ArrayBuffer>;
-    fileName: string;
-    isPublic?: boolean;
-    expirationTime?: string;
-    pollInterval?: number;
-    timeout?: number;
-    signaturePrompter?: TSignaturePrompter;
-    txPrompter?: TxPrompter;
+  fileBytes: Uint8Array<ArrayBuffer>;
+  fileName: string;
+  isPublic?: boolean;
+  expirationTime?: string;
+  pollInterval?: number;
+  timeout?: number;
+  signaturePrompter?: TSignaturePrompter;
+  txPrompter?: TxPrompter;
 }
